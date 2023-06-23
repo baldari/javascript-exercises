f7505370cdee717b62e14e6909fbcbea33b2fdfa
Matthew Baldari <baldarimatt@gmail.com>
2023-06-23 13:27:53 -0400

    4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    const array = [];
+    arr.forEach((element) => {
+        if (!args.includes(element)) {
+            array.push(element);
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
