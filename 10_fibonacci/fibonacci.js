9bc79beb2fe648793401b4b177cf38e38a027b17
Matthew Baldari <baldarimatt@gmail.com>
2023-06-23 15:02:34 -0400

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num == 1) {
+        return 1;
+    }
+    let x = 1;
+    let y = 0;
+    let sum;
+    for (let i = 0; i < num - 1; i++) {
+        sum = x + y;
+        let temp = x;
+        x = sum;
+        y = temp;
+    }
+    return sum;
 };
 
 // Do not edit below this line
