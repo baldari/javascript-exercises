141e60f6aa6cc3b8d6b84d6ccaf12df59ea08299
Matthew Baldari <baldarimatt@gmail.com>
2023-06-23 14:16:24 -0400

    8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let mult = 1;
+  for (let i = 0; i < args.length; i++) {
+    mult *= args[i];
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1;
+  }
+	return num * factorial(num - 1);
 };
 
 // Do not edit below this line
