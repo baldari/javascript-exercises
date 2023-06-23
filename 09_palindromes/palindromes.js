4a0ae96642c6cbd56f7c154c3e34ac5d171742b1
Matthew Baldari <baldarimatt@gmail.com>
2023-06-23 14:28:54 -0400

    9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let checker = "abcdefghijklmnopqrstuvwxyz1234567890"
+    let x = 0;
+    let y = str.length - 1;
+    while (y > x) {
+        if (checker.includes(str[x]) == false) {
+            x++;
+            continue;
+        }
+        if (checker.includes(str[y]) == false) {
+            y--;
+            continue;
+        }
+        if (str[x] != str[y]) {
+            return false;
+        }
+        x++;
+        y--;
+    }
+    return true;
 };
 
 // Do not edit below this line
