93baeb88fa00030937f7628722ddd12efab1b289
Matthew Baldari <baldarimatt@gmail.com>
2023-06-23 12:57:17 -0400

    2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) {
+    return "ERROR"
+  }
+  let newstr = "";
+  for (let i = 0; i < num; i++) {
+    newstr += str;
+  }
+  return newstr;
 };
 
 // Do not edit below this line
