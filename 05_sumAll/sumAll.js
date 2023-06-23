7b2f85d5a448f34dedc9e2a3c5ab9a06f621e041
Matthew Baldari <baldarimatt@gmail.com>
2023-06-23 13:40:10 -0400

    5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    if (!Number.isInteger(start) || !Number.isInteger(stop)) {
+        return "ERROR";
+    }
+    if (start < 0 || stop < 0) {
+        return "ERROR";
+    }
+    if (stop < start) {
+        let temp = stop;
+        stop = start;
+        start = temp;
+    }
+    let sum = 0;
+    while (start <= stop) {
+        sum += start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
